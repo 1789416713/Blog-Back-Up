6dabec27d5dfe08d53df1d3827faeb460c0e48e6
zero <1789416713@qq.com>
2018-09-04 00:23:14 +0800

    add photos

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/lb948174033/blog-back-up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/lb948174033/blog-back-up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/1789416713/Blog-Back-Up/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/1789416713/Blog-Back-Up/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
